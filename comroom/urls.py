ce1e28e28e34e371760efc43f3b95c7d82e987f8
ssamko <teacher.ssamko@gmail.com>
2020-02-28 11:43:42 +0900

    register form

--- comroom/urls.py
+++ comroom/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from school.views import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RegisterView.as_view()),
 ]
--- school/forms.py
+++ school/forms.py
@@ -4,28 +4,31 @@ from .models import School, AdminUser
 
 
 class RegisterForm(forms.Form):
-    province = forms.ChoiceField(choices=(
-        ('서울특별시교육청', '서울'),
-        ('부산광역시교육청', '부산'),
-        ('대구광역시교육청', '대구'),
-        ('인천광역시교육청', '인천'),
-        ('광주광역시교육청', '광주'),
-        ('대전광역시교육청', '대전'),
-        ('울산광역시교육청', '울산'),
-        ('세종특별자치시교육청', '세종'),
-        ('경기도교육청', '경기'),
-        ('강원도교육청', '강원'),
-        ('충청북도교육청', '충북'),
-        ('충청남도교육청', '충남'),
-        ('전라북도교육청', '전북'),
-        ('전라남도교육청', '전남'),
-        ('경상북도교육청', '경북'),
-        ('경상남도교육청', '경남'),
-        ('제주특별자치도교육청', '제주'),
-    ), label='교육청',
-        error_messages={
-        'required': '교육청을 선택해주세요.'
-    })
+    province = forms.CharField(label='교육청',
+                               widget=forms.Select(
+                                   choices=[
+                                       ('서울특별시교육청', '서울'),
+                                       ('부산광역시교육청', '부산'),
+                                       ('대구광역시교육청', '대구'),
+                                       ('인천광역시교육청', '인천'),
+                                       ('광주광역시교육청', '광주'),
+                                       ('대전광역시교육청', '대전'),
+                                       ('울산광역시교육청', '울산'),
+                                       ('세종특별자치시교육청', '세종'),
+                                       ('경기도교육청', '경기'),
+                                       ('강원도교육청', '강원'),
+                                       ('충청북도교육청', '충북'),
+                                       ('충청남도교육청', '충남'),
+                                       ('전라북도교육청', '전북'),
+                                       ('전라남도교육청', '전남'),
+                                       ('경상북도교육청', '경북'),
+                                       ('경상남도교육청', '경남'),
+                                       ('제주특별자치도교육청', '제주'),
+                                   ]
+                               ),
+                               error_messages={
+                                   'required': '교육청을 선택해주세요.'
+                               })
     name = forms.CharField(
         error_messages={
             'required': '학교명을 입력해주세요.'
@@ -48,13 +51,13 @@ class RegisterForm(forms.Form):
         error_messages={
             'required': '비밀번호를 입력해주세요'
         },
-        widget=forms.PasswordInput, lable='비밀번호'
+        widget=forms.PasswordInput, label='비밀번호'
     )
     re_password = forms.CharField(
         error_messages={
             'required': '비밀번호를 다시 입력해주세요'
         },
-        widget=forms.PasswordInput, lable='비밀번호 확인'
+        widget=forms.PasswordInput, label='비밀번호 확인'
     )
     realname = forms.CharField(
         error_messages={
