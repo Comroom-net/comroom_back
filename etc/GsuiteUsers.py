c318fdee85bd99a59fc2e11bd75b7ab35852f479
ssamko <ssamko@sam-yang-yeongsang-ui-iMac.local>
2020-05-01 14:43:44 +0900

    g-suite parent class

--- etc/GsuiteUsers.py
+++ etc/GsuiteUsers.py
@@ -5,42 +5,17 @@ from django.core.files import File
 
 from openpyxl import Workbook, load_workbook
 
-
-class GUser:
-    def __init__(self, file, s_info, grade, classN):
-        self.get_properties(file, s_info, grade, classN)
-        self.init_data()
-        self.put_data()
-        self.make_csv()
-        self.get_url()
-
-    def get_properties(self, file, s_info, grade, classN):
-        # xlsx 파일에서 학생정보 가져오기
-        wb = load_workbook(file)
-        ws = wb[wb.sheetnames[0]]
-        for max_row, row in enumerate(ws, 1):
-            if all(c.value is None for c in row):
-                break
-        print('ws.max row:', ws.max_row)
-        max_row -= 1
-        print('max row:', max_row)
+class Gsuite:
+    def __init__(self):
         self.students = []
-        for r in range(2, max_row + 1):
-            self.students.append(ws.cell(row=r, column=2).value)
-
-        print(self.students)
-        # Properties
-        self.s_code, self.school, self.s_admin = s_info
-        self.grade = grade
-        self.class_ = classN
-
-        thisYear = datetime.now().strftime("%Y")
-        self.yearCode = thisYear[2:]
-
-        self.samplePW = f'{self.s_admin}{self.s_code}'
-        if len(self.samplePW) < 8:
-            self.samplePW += '!'
-        self.orgPath = f'/{self.school}'
+        self.s_code = ''
+        self.yearCode = ''
+        self.grade = ''
+        self.class_ = ''
+        self.s_num = 0
+        self.samplePW = ''
+        self.orgPath = ''
+        self.s_admin = ''
 
     def init_data(self):
         # data for school_user.csv
@@ -81,3 +56,42 @@ class GUser:
         with open(self.result_file, 'r') as f:
             tmpfile = File(f)
             self.file_url = '/'+tmpfile.name
+
+
+class GUser(Gsuite):
+    def __init__(self, file, s_info, grade, classN):
+        self.get_properties(file, s_info, grade, classN)
+        self.init_data()
+        self.put_data()
+        self.make_csv()
+        self.get_url()
+
+    def get_properties(self, file, s_info, grade, classN):
+        # xlsx 파일에서 학생정보 가져오기
+        wb = load_workbook(file)
+        ws = wb[wb.sheetnames[0]]
+        for max_row, row in enumerate(ws, 1):
+            if all(c.value is None for c in row):
+                break
+        print('ws.max row:', ws.max_row)
+        max_row -= 1
+        print('max row:', max_row)
+        self.students = []
+        for r in range(2, max_row + 1):
+            self.students.append(ws.cell(row=r, column=2).value)
+
+        print(self.students)
+        # Properties
+        self.s_code, self.school, self.s_admin = s_info
+        self.grade = grade
+        self.class_ = classN
+
+        thisYear = datetime.now().strftime("%Y")
+        self.yearCode = thisYear[2:]
+
+        self.samplePW = f'{self.s_admin}{self.s_code}'
+        if len(self.samplePW) < 8:
+            self.samplePW += '!'
+        self.orgPath = f'/{self.school}'
+
+    
