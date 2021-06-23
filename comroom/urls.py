9225bff2b466e867682a00558246541b6f694bba
ssamko <teacher.ssamko@gmail.com>
2021-06-23 21:25:16 +0900

    comroom: urls: add swagger-ui

--- comroom/urls.py
+++ comroom/urls.py
@@ -1,51 +1,48 @@
-"""comroom URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
-from school.views import index, AboutView, ComroomAdminView, make_room, reset_password, \
-    send_password_mail
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+from school.views import (
+    index,
+    AboutView,
+    ComroomAdminView,
+    make_room,
+    reset_password,
+    send_password_mail,
+)
 from timetable.views import assign_room
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('school/', include('school.urls')),
-    path('etc/', include('etc.urls')),
-    path('afterschool/', include('afterschool.urls')),
-    path('namu/', include('namu.urls')),
+    path("admin/", admin.site.urls),
+    path("", index, name="index"),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("school/", include("school.urls")),
+    path("etc/", include("etc.urls")),
+    path("afterschool/", include("afterschool.urls")),
+    path("namu/", include("namu.urls")),
     # path('make_room/', make_room),
     # path('assign_room/', assign_room),
-    path('send_password_mail/', send_password_mail, name='send_password_mail'),
-    path('reset_password/<str:token>/', reset_password),
-    path('comroom_admin/', ComroomAdminView.as_view()),
-    path('ssam_ko/', AboutView.as_view(), name='about'),
-    path('howto/', TemplateView.as_view(template_name="howto.html")),
-    path('whatis/', TemplateView.as_view(template_name="whatis.html")),
-    path('FAQ/', TemplateView.as_view(template_name="faq.html")),
-    path('db_reset/', TemplateView.as_view(template_name="dbreset.html")),
-    path('timetable/', include('timetable.urls', namespace='timetable')),
-
+    path("send_password_mail/", send_password_mail, name="send_password_mail"),
+    path("reset_password/<str:token>/", reset_password),
+    path("comroom_admin/", ComroomAdminView.as_view()),
+    path("ssam_ko/", AboutView.as_view(), name="about"),
+    path("howto/", TemplateView.as_view(template_name="howto.html")),
+    path("whatis/", TemplateView.as_view(template_name="whatis.html")),
+    path("FAQ/", TemplateView.as_view(template_name="faq.html")),
+    path("db_reset/", TemplateView.as_view(template_name="dbreset.html")),
+    path("timetable/", include("timetable.urls", namespace="timetable")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATICFILES_DIRS)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
