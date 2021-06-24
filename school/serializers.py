62c2786706df948255ecb227779c434e5ce75631
ssamko <teacher.ssamko@gmail.com>
2021-06-24 22:20:34 +0900

    school: serializers: add all serializers

--- school/serializers.py
+++ school/serializers.py
@@ -2,7 +2,26 @@ from rest_framework import serializers
 
 from .models import School, Comroom, AdminUser, Notice
 
+
 class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
-        fields = '__all__'
+        fields = "__all__"
+
+
+class ComroomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comroom
+        fields = "__all__"
+
+
+class AdminUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdminUser
+        fields = "__all__"
+
+
+class NoticeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notice
+        fields = "__all__"
