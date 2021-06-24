435649a15a7c6b3d9815b0f76909a6ac4a7c54fa
ssamko <teacher.ssamko@gmail.com>
2021-06-24 22:10:30 +0900

    settings: add cors-header

--- comroom/settings/local.py
+++ comroom/settings/local.py
@@ -4,3 +4,10 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+
+# CORS_ALLOW_ALL_ORIGINS = True
