f183f8e3251f16e45b4d02c44d293ecf9b793b44
ssamko <teacher.ssamko@gmail.com>
2021-08-03 08:07:22 +0900

    timetable: urls: add valid_scode_api

--- timetable/urls.py
+++ timetable/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .views import (
+    valid_scode_api,
     time_admin,
     FixCreateView,
     del_time,
@@ -21,6 +22,7 @@ router.register(r"fixed", FixedTimetableViewSet)
 
 app_name = "timetable"
 urlpatterns = [
+    path("api/", valid_scode_api),
     path("api/", include(router.urls)),
     path("time_admin/", time_admin),
     path("fix_time/", FixCreateView.as_view()),
