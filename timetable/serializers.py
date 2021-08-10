2c42fe9a4ffaacf7501c2c1edb9bd921abc7747c
ssamko <teacher.ssamko@gmail.com>
2021-08-10 23:18:20 +0900

    comroom: timetable: serializer: Use mixed field

--- timetable/serializers.py
+++ timetable/serializers.py
@@ -1,6 +1,11 @@
+import logging
+
 from rest_framework import serializers
 
 from .models import Timetable, FixedTimetable
+from school.models import Comroom
+
+logger = logging.getLogger(__name__)
 
 
 class TimetableSerializer(serializers.ModelSerializer):
@@ -10,7 +15,10 @@ class TimetableSerializer(serializers.ModelSerializer):
 
 
 class FixedTimetableSerializer(serializers.ModelSerializer):
-    comroom = serializers.StringRelatedField()
+    comroom = serializers.StringRelatedField(read_only=True)
+    comroom_id = serializers.PrimaryKeyRelatedField(
+        queryset=Comroom.objects.all(), write_only=True, source="comroom"
+    )
 
     class Meta:
         model = FixedTimetable
