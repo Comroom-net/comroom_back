6848936aac4fa81585d818abc8290fe146c78309
ssamko <teacher.ssamko@gmail.com>
2021-08-11 23:24:15 +0900

    settings.staging: add cors allowed origins

--- comroom/settings/staging.py
+++ comroom/settings/staging.py
@@ -1,7 +1,14 @@
 from .base import *
 
+
 DEBUG = True
+
+WEB_URL = "http://dev.comroom.net/"
 
 ALLOWED_HOSTS = ["*"]
 
-WEB_URL = "http://localhost:8080/"
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "http://dev.comroom.net/",
+]
