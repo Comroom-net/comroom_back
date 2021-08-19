99c854355f78ed4e1676e359bc497caac29be7df
ssamko <teacher.ssamko@gmail.com>
2021-08-19 23:34:21 +0900

    settings: for cors

--- comroom/settings/production.py
+++ comroom/settings/production.py
@@ -5,3 +5,7 @@ DEBUG = False
 ALLOWED_HOSTS = ["comroom.net", "api.comroom.net"]
 
 WEB_URL = "http://comroom.net/"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://comroom.net",
+]
--- comroom/settings/staging.py
+++ comroom/settings/staging.py
@@ -11,4 +11,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
     "http://dev.comroom.net",
+    "http://comroom.net",
 ]
